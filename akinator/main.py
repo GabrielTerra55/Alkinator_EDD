0d14eb83fed9a4a6c76a324901aa4f963ea2ec56
Sandro Santana Ribeiro <sandro.santana@grad.ufsc.br>
2023-06-02 13:42:30 -0300

    conserta main

--- akinator/main.py
+++ akinator/main.py
@@ -1,5 +1,4 @@
 from controller.controller_principal import ControllerPrincipal
 
 if __name__ == "__main__":
-    akinator = ControllerPrincipal()
-    akinator.inicializa_controller_principal()
+    akinator = ControllerPrincipal().inicializa_controller_principal()
